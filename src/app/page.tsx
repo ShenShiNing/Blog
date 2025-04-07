3fc69fd91c1c2819bc7833a3e2f661cce24cb61f
Shen Shining <353824385@qq.com>
2025-04-07 12:50:07 +0800

    fix isr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,12 @@ import AboutSection from "@/modules/sections/about/about-section";
 import ContactSection from "@/modules/sections/contact/contact-section";
 import { getAllBlogs } from "@/lib/blog";
 import { getAllPortfolios } from "@/lib/portfolio";
-import { Blog } from "@/types/blog";
-import { Portfolio } from "@/types/portfolio";
 
-export const dynamicParams = true;
 export const revalidate = 604800;
-export const generateStaticParams = async () => {
-  const blogs = await getAllBlogs();
-  const portfolios = await getAllPortfolios();
-  return [{ blogs }, { portfolios }];
-};
 
-const Page = async ({
-  params,
-}: {
-  params: Promise<{ blogs: Blog[]; portfolios: Portfolio[] }>;
-}) => {
-  const { blogs, portfolios } = await params;
-
+const Page = async () => {
+  const blogs = (await getAllBlogs()) || [];
+  const portfolios = (await getAllPortfolios()) || [];
   return (
     <main>
       <HeroSection />
